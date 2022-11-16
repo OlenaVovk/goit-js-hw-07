494887ffad27449853cd3fbcbcc14ab4fbc9cb52
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-11-16 23:02:47 +0200

    half 1gallery + 3gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -3,15 +3,38 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const divEl = document.querySelector('div.gallery');
 
 const renderList = (images) => 
     images.reduce((acc, {preview, original, description}) => 
     acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
 
-
 const insertListItems = (string => {
-    const divEl = document.querySelector('div.gallery');
     divEl.insertAdjacentHTML('afterbegin', string);
 });
 
 insertListItems(renderList(galleryItems));
+
+divEl.addEventListener('click', clickHendler);
+
+function clickHendler (evt){
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG"){
+        return;
+    }
+
+    const images = document.querySelectorAll('img');
+
+    images.forEach((image) =>{
+    
+    const instance = basicLightbox.create(`<div class="modal">
+        <img src="${evt.target.dataset.source}" alt="Big img"/>
+    </div>`, {closable: true});
+    instance.show();
+    });
+   
+}
+ 
+
+
--- js/03-mygallery.js
+++ js/03-mygallery.js
@@ -3,14 +3,47 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
+const ulEl = document.querySelector('ul.gallery');
+
 const renderList = (images) => 
     images.reduce((acc, {preview, original, description}) => 
     acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
 
-
-const insertListItems = (string => {
-    const ulEl = document.querySelector('ul.gallery');
-    ulEl.insertAdjacentHTML('afterbegin', string);
+const insertListItems = ((el,string) => {
+    el.insertAdjacentHTML('afterbegin', string);
 });
 
-insertListItems(renderList(galleryItems));
+insertListItems(ulEl, renderList(galleryItems));
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+
+document.body.appendChild(lightbox);
+
+ulEl.addEventListener('click', clickHendler);
+
+function clickHendler (evt){
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG"){
+        return;
+    }
+    const images = document.querySelectorAll('img');
+
+    images.forEach(() => {
+        lightbox.classList.add('active');
+        const img = document.createElement('img');
+        img.src = evt.target.dataset.source;
+        while(lightbox.firstChild){
+             lightbox.removeChild(lightbox.firstChild);
+        }
+        lightbox.append(img);
+    })
+}
+
+lightbox.addEventListener('click', (evt) => {
+    if (evt.target.nodeName !== "IMG"){
+        return;
+    }
+    lightbox.classList.remove('active');
+});
