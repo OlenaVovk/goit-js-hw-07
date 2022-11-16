3494d05524294b817d244b6f1487b9c5a574031d
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-11-16 19:36:38 +0200

    renderList

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,15 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const renderList = (images) => 
+    images.reduce((acc, {preview, original, description}) => 
+    acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
+
+
+const insertListItems = (string => {
+    const ulEl = document.querySelector('ul.gallery');
+    ulEl.insertAdjacentHTML('afterbegin', string);
+});
+
+insertListItems(renderList(galleryItems));
--- js/03-mygallery.js
+++ js/03-mygallery.js
@@ -3,15 +3,14 @@ import { galleryItems } from './gallery-items.js';
 
 console.log(galleryItems);
 
-
 const renderList = (images) => 
     images.reduce((acc, {preview, original, description}) => 
     acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
 
 
 const insertListItems = (string => {
-    const divEl = document.querySelector('div.gallery');
-    divEl.insertAdjacentHTML('afterbegin', string);
+    const ulEl = document.querySelector('ul.gallery');
+    ulEl.insertAdjacentHTML('afterbegin', string);
 });
 
 insertListItems(renderList(galleryItems));
