bf6bc5fee8d6be240b1c47d8d679bad826021907
OlenaVovk <109525532+OlenaVovk@users.noreply.github.com>
2022-11-17 20:19:47 +0200

    02gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -5,12 +5,33 @@ console.log(galleryItems);
 
 const renderList = (images) => 
     images.reduce((acc, {preview, original, description}) => 
-    acc + `<a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a>`, '');
+    acc + `<a class="gallery__item" href="${original}">
+    <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`, '');
 
+const ulEl = document.querySelector('ul.gallery');
 
 const insertListItems = (string => {
-    const ulEl = document.querySelector('ul.gallery');
     ulEl.insertAdjacentHTML('afterbegin', string);
 });
 
 insertListItems(renderList(galleryItems));
+
+ulEl.addEventListener('click', clickHendler);
+
+function clickHendler (evt){
+    evt.preventDefault();
+
+    if (evt.target.nodeName !== "IMG"){
+        return;
+    }
+
+   let gallery = new SimpleLightbox('.gallery a', {
+    captions: true,
+    captionType: 'text',
+    captionsData: 'fghfghjklhjl;mjjk',
+    captionDelay: 250,
+   });
+    gallery.on('show.simplelightbox');
+
+}
